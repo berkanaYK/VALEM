7504c947de860d634f907aed5d355e5abe152169
Berkan <121131831+berkanaYK@users.noreply.github.com>
2026-08-17 15:14:29 +0300

    Add Android main activity

---
+++ src/VALE.Mobile/Platforms/Android/MainActivity.cs
@@ -0,0 +1,12 @@
+using Android.App;
+using Android.Content.PM;
+using Microsoft.Maui;
+
+namespace VALE.Mobile;
+
+[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true,
+    ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode |
+                           ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+public sealed class MainActivity : MauiAppCompatActivity
+{
+}
