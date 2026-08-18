8863066f92694b2067c3675179a19317dbd7f625
Berkan <121131831+berkanaYK@users.noreply.github.com>
2026-08-18 12:31:45 +0300

    Validate positional record contract metadata correctly

--- tests/VALE.Api.Tests/AuthContractTests.cs
+++ tests/VALE.Api.Tests/AuthContractTests.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using System.Reflection;
 using VALE.Contracts;
 using Xunit;
 
@@ -9,27 +10,30 @@ public sealed class AuthContractTests
     [Fact]
     public void Two_factor_code_requires_six_digits()
     {
-        var invalid = new TwoFactorCodeRequest("12345");
-        var context = new ValidationContext(invalid);
-        var results = new List<ValidationResult>();
-        Assert.False(Validator.TryValidateObject(invalid, context, results, validateAllProperties: true));
+        Assert.False(ConstructorParameterIsValid<TwoFactorCodeRequest>(0, "12345"));
+        Assert.True(ConstructorParameterIsValid<TwoFactorCodeRequest>(0, "123456"));
     }
 
     [Fact]
     public void Profile_color_requires_hex_rgb()
     {
-        var invalid = new UpdateAccountProfileRequest("Test User", null, "System", "Blue", "blue");
-        var context = new ValidationContext(invalid);
-        var results = new List<ValidationResult>();
-        Assert.False(Validator.TryValidateObject(invalid, context, results, validateAllProperties: true));
+        Assert.False(ConstructorParameterIsValid<UpdateAccountProfileRequest>(4, "blue"));
+        Assert.True(ConstructorParameterIsValid<UpdateAccountProfileRequest>(4, "#2563EB"));
     }
 
     [Fact]
     public void Ticket_delete_requires_reason()
     {
-        var invalid = new DeleteTicketRequest("x");
-        var context = new ValidationContext(invalid);
-        var results = new List<ValidationResult>();
-        Assert.False(Validator.TryValidateObject(invalid, context, results, validateAllProperties: true));
+        Assert.False(ConstructorParameterIsValid<DeleteTicketRequest>(0, "x"));
+        Assert.True(ConstructorParameterIsValid<DeleteTicketRequest>(0, "Yanlış plaka ile açıldı"));
+    }
+
+    private static bool ConstructorParameterIsValid<T>(int parameterIndex, object? value)
+    {
+        var constructor = typeof(T).GetConstructors(BindingFlags.Public | BindingFlags.Instance).Single();
+        var parameter = constructor.GetParameters()[parameterIndex];
+        var attributes = parameter.GetCustomAttributes<ValidationAttribute>(inherit: true).ToArray();
+        Assert.NotEmpty(attributes);
+        return attributes.All(attribute => attribute.IsValid(value));
     }
 }
