b679565fab70ff0445d7f701041e367bb130d997
Berkan <121131831+berkanaYK@users.noreply.github.com>
2026-08-18 13:51:43 +0300

    Use clean bottom navigation shell on Android

--- src/VALE.Mobile/App.cs
+++ src/VALE.Mobile/App.cs
@@ -11,19 +11,23 @@ public sealed class App : Application
         ThemeService.ApplyStored(this);
         RequestedThemeChanged += (_, _) =>
         {
-            if (ThemeService.CurrentMode == ValeThemeMode.System) ThemeService.Apply(ValeThemeMode.System, ThemeService.CurrentAccent);
+            if (ThemeService.CurrentMode == ValeThemeMode.System)
+                ThemeService.Apply(ValeThemeMode.System, ThemeService.CurrentAccent);
         };
     }
 
-    protected override Window CreateWindow(IActivationState? activationState) => new(new NavigationPage(new MainPage()));
+    protected override Window CreateWindow(IActivationState? activationState) =>
+        new(new NavigationPage(new MainPage()));
 
     public static void ShowAuthenticated(ApiClient api, UserDto user)
     {
-        if (Current?.Windows.FirstOrDefault() is { } window) window.Page = new CompanyMainTabsPage(api, user);
+        if (Current?.Windows.FirstOrDefault() is { } window)
+            window.Page = new ValeAppShell(api, user);
     }
 
     public static void ShowLogin()
     {
-        if (Current?.Windows.FirstOrDefault() is { } window) window.Page = new NavigationPage(new MainPage());
+        if (Current?.Windows.FirstOrDefault() is { } window)
+            window.Page = new NavigationPage(new MainPage());
     }
 }
